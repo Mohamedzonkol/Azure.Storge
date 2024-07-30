6a61c5d68f59133ddbd6da9cc6e869237d9c987e
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-07-30 18:35:40 +0300

    add controller

--- Azure.Storge/Controllers/AttendeeRegistrationController.cs
+++ Azure.Storge/Controllers/AttendeeRegistrationController.cs
@@ -1,12 +1,143 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure.Storge.Data;
+using Azure.Storge.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Azure.Storge.Controllers
 {
-    public class AttendeeRegistrationController : Controller
+    public class AttendeeRegistrationController(ITableStorageService tableStorageService
+        /*IBlobStorageService blobStorageService, IQueueService queueService*/) : Controller
     {
-        public IActionResult Index()
+        public async Task<ActionResult> Index()
+        {
+            var data = await tableStorageService.GetAttendees();
+            foreach (var item in data)
+            {
+                item.ImageName = await _blobStorageService.GetBlobUrl(item.ImageName);
+            }
+            return View(data);
+        }
+        public async Task<ActionResult> Details(string id, string industry)
+        {
+            var data = await tableStorageService.GetAttendee(industry, id);
+            data.ImageName = await _blobStorageService.GetBlobUrl(data.ImageName);
+            return View(data);
+        }
+        public ActionResult Create()
         {
             return View();
+        }
+
+        // POST: AttendeeRegistrationController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(AttendeeEntity attendeeEntity,
+            IFormFile formFile)
+        {
+            try
+            {
+                var id = Guid.NewGuid().ToString();
+                attendeeEntity.PartitionKey = attendeeEntity.Industry;
+                attendeeEntity.RowKey = id;
+
+                if (formFile?.Length > 0)
+                {
+                    attendeeEntity.ImageName =
+                        await _blobStorageService.UploadBlob(formFile, id);
+                }
+                else
+                {
+                    attendeeEntity.ImageName = "default.jpg";
+                }
+
+                await tableStorageService.UpsertAttendee(attendeeEntity);
+
+                var email = new EmailMessage
+                {
+                    EmailAddress = attendeeEntity.EmailAddress,
+                    TimeStamp = DateTime.UtcNow,
+                    Message = $"Hello {attendeeEntity.FirstName} {attendeeEntity.LastName}," +
+                    $"\n\r Thank you for registering for this event. " +
+                    $"\n\r Your record has been saved for future reference. "
+                };
+                await _queueService.SendMessage(email);
+
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AttendeeRegistrationController/Edit/5
+        public async Task<ActionResult> Edit(string id, string industry)
+        {
+            var data = await tableStorageService.GetAttendee(industry, id);
+
+            return View(data);
+        }
+
+        // POST: AttendeeRegistrationController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(AttendeeEntity attendeeEntity,
+            IFormFile formFile)
+        {
+            try
+            {
+                if (formFile?.Length > 0)
+                {
+                    attendeeEntity.ImageName = await _blobStorageService.UploadBlob(formFile, attendeeEntity.RowKey, attendeeEntity.ImageName);
+                }
+
+                attendeeEntity.PartitionKey = attendeeEntity.Industry;
+
+                await tableStorageService.UpsertAttendee(attendeeEntity);
+
+                var email = new EmailMessage
+                {
+                    EmailAddress = attendeeEntity.EmailAddress,
+                    TimeStamp = DateTime.UtcNow,
+                    Message = $"Hello {attendeeEntity.FirstName} {attendeeEntity.LastName}," +
+                    $"\n\r Your record was modified successfully"
+                };
+                await _queueService.SendMessage(email);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // POST: AttendeeRegistrationController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(string id, string industry)
+        {
+            try
+            {
+                var data = await tableStorageService.GetAttendee(industry, id);
+                await tableStorageService.DeleteAttendee(industry, id);
+                await _blobStorageService.RemoveBlob(data.ImageName);
+
+                var email = new EmailMessage
+                {
+                    EmailAddress = data.EmailAddress,
+                    TimeStamp = DateTime.UtcNow,
+                    Message = $"Hello {data.FirstName} {data.LastName}," +
+                    $"\n\r Your record was removed successfully"
+                };
+                await _queueService.SendMessage(email);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }

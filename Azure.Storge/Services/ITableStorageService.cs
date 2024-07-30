a769179180e74b196a9bcd0b39eeaaeb605c675b
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-07-30 18:19:00 +0300

    Create First Service

--- Azure.Storge/Services/ITableStorageService.cs
+++ Azure.Storge/Services/ITableStorageService.cs
@@ -1,6 +1,12 @@
-﻿namespace Azure.Storge.Services
+﻿using Azure.Storge.Data;
+
+namespace Azure.Storge.Services
 {
-    public class ITableStorageService
+    public interface ITableStorageService
     {
+        Task DeleteAttendee(string industry, string id);
+        Task<AttendeeEntity> GetAttendee(string industry, string id);
+        Task<List<AttendeeEntity>> GetAttendees();
+        Task UpsertAttendee(AttendeeEntity attendeeEntity);
     }
 }
---
+++ Azure.Storge/Services/TableStorageService.cs
@@ -0,0 +1,27 @@
+﻿using Azure.Storge.Data;
+
+namespace Azure.Storge.Services
+{
+    public class TableStorageService : ITableStorageService
+    {
+        public Task DeleteAttendee(string industry, string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AttendeeEntity> GetAttendee(string industry, string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<AttendeeEntity>> GetAttendees()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpsertAttendee(AttendeeEntity attendeeEntity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

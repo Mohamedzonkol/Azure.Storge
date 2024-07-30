3b826ed5ced12913d9b47c2c0698e9848c4710a2
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-07-30 18:46:32 +0300

    complete all services

--- Azure.Storge/Services/BlobStorageService.cs
+++ Azure.Storge/Services/BlobStorageService.cs
@@ -1,20 +1,71 @@
-﻿namespace Azure.Storge.Services
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Sas;
+
+namespace Azure.Storge.Services
 {
-    public class BlobStorageService : IBlobStorageService
+
+    public class BlobStorageService(IConfiguration configuration, BlobServiceClient blobServiceClient) : IBlobStorageService
     {
-        public Task<string> GetBlobUrl(string imageName)
+        private string containerName = "attendeeimages";
+
+        public async Task<string> GetBlobUrl(string imageName)
         {
-            throw new NotImplementedException();
+            var container = blobServiceClient.GetBlobContainerClient(containerName);
+            var blob = container.GetBlobClient(imageName);
+
+            BlobSasBuilder blobSasBuilder = new()
+            {
+                BlobContainerName = blob.BlobContainerName,
+                BlobName = blob.Name,
+                ExpiresOn = DateTime.UtcNow.AddMinutes(2),
+                Protocol = SasProtocol.Https,
+                Resource = "b"
+            };
+            blobSasBuilder.SetPermissions(BlobAccountSasPermissions.Read);
+
+            return blob.GenerateSasUri(blobSasBuilder).ToString();
         }
 
-        public Task RemoveBlob(string imageName)
+        public async Task RemoveBlob(string imageName)
         {
-            throw new NotImplementedException();
+            var container = blobServiceClient.GetBlobContainerClient(containerName);
+            var blob = container.GetBlobClient(imageName);
+            await blob.DeleteIfExistsAsync(DeleteSnapshotsOption.IncludeSnapshots);
         }
 
-        public Task<string> UploadBlob(IFormFile formFile, string imageName, string? originalBlobName = null)
+        public async Task<string> UploadBlob(IFormFile formFile, string imageName, string? originalBlobName = null)
         {
-            throw new NotImplementedException();
+            var blobName = $"{imageName}{Path.GetExtension(formFile.FileName)}";
+            var container = blobServiceClient.GetBlobContainerClient(containerName); ;
+
+            if (!string.IsNullOrEmpty(originalBlobName))
+            {
+                await RemoveBlob(originalBlobName);
+            }
+
+            using var memoryStream = new MemoryStream();
+            await formFile.CopyToAsync(memoryStream);
+            memoryStream.Position = 0;
+            var blob = container.GetBlobClient(blobName);
+            await blob.UploadAsync(content: memoryStream, overwrite: true);
+            return blobName;
+        }
+        private async Task<BlobContainerClient> GetBlobContainerClient()
+        {
+            try
+            {
+                BlobContainerClient container = new BlobContainerClient(configuration["StorageConnectionString"], containerName);
+                await container.CreateIfNotExistsAsync();
+
+                return container;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
         }
     }
 }
